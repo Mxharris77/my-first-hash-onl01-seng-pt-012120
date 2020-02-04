010e7dbd2acc86a506f351845cd948fe351b7203
captivating-mapping-7374 <mxharris1094@yahoo.com>
2020-02-04 00:29:05 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
- identification = { "name" => "Malcolm" , "age" => 26 , "favorite color" => "red"}
+ me = { "name" => "Malcolm" , "age" => 26 , "favorite color" => "red"}
   
 end
 
